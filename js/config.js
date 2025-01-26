bc531c9846409f2dea2bf6c9e1125c5d45e334e1
Leoni Gallant <116727639+Leoni-Gallant@users.noreply.github.com>
2025-01-26 15:18:18 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_1987lh8aV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '691jmamimme6hmbot64mfv4ds',// e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4lt09dhogi4m8hfbpgobrh7i55',// e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
